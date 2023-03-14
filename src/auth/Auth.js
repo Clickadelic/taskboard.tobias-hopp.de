ec11caf6c9d0453789eeaaa41b35a45f53988b14
Clickadelic <toby.hopp@gmail.com>
2023-03-14 17:31:16 +0100

    Kommentar hinzugefügt

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-// TODO ReWrite these components to PublicHeader
+// TODO ReWrite these components to Public Header with Navbar
 const Auth = () => {
     return (
         <div className="cool-background flex h-screen">        
