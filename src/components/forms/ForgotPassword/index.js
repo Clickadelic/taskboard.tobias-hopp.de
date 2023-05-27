069fb0b724da9db7fa934217945ad425876ba71c
clickadelic <toby.hopp@gmail.com>
2023-05-28 01:07:27 +0200

    Changes

--- src/components/forms/ForgotPassword/index.js
+++ src/components/forms/ForgotPassword/index.js
@@ -24,14 +24,15 @@ function ForgotPassword(){
 		e.preventDefault()
 	}
 	return(
-		<section className="bg-white p-12 rounded">
+		<section className="bg-white p-4 rounded">
+			<h1 className="text-2xl mb-4">Forgot Password</h1>
 			<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
 			<form className="w-full" onSubmit={handleSubmit}>
 				<div className="mb-4">
-					<label htmlFor="username" className="flex text-medium mb-2">E-Mail:</label>
-					<input type="text" id="username" className="block p-3 text-medium rounded w-full" autoComplete="off" placeholder="E-Mail Adresse" onChange={(e)=> setUser(e.target.value)} value={user} required />
+					<label htmlFor="username" className="flex text-medium mb-2 font-bold">E-Mail:</label>
+					<input type="text" id="username" className="block p-3 bg-slate-200 text-lg rounded w-full mb-3" autoComplete="off" placeholder="E-Mail Adresse" onChange={(e)=> setUser(e.target.value)} value={user} required />
 				</div>
-				<button className="rounded bg-blue-600 hover:bg-blue-700 px-3 p-4 text-medium w-full text-white">Passwort wiederherstellen</button>
+				<button className="rounded bg-sky-600 hover:bg-sky-700 p-2 text-lg w-full text-white">Passwort wiederherstellen</button>
 			</form>
 		</section>
 	)
