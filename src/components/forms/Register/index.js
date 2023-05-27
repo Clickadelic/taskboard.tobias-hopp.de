74d97fe38d8ccfce6844e4459b19d509f0eeaf49
clickadelic <toby.hopp@gmail.com>
2023-05-28 01:04:16 +0200

    Stylechanges

--- src/components/forms/Register/index.js
+++ src/components/forms/Register/index.js
@@ -107,12 +107,12 @@ const Register = () => {
 								ref={userRef}
 								autoComplete="off"
 								onChange={(e) => setUser(e.target.value)}
-								value={user}
 								required
 								aria-invalid={validName ? "false" : "true"}
 								aria-describedby="uidnote"
 								onFocus={() => setUserFocus(true)}
 								onBlur={() => setUserFocus(false)}
+								placeholder="Username"
 								className="block p-3 bg-slate-200 text-lg rounded w-full mb-3"
 							/>
 							<p id="uidnote" className={userFocus && user && !validName ? "block p-2 rounded bg-red-100" : "hidden"}>
@@ -120,36 +120,37 @@ const Register = () => {
 								4 to 24 characters. Must begin with a letter. Letters, numbers, underscores, hyphens allowed.
 							</p>
 						</div>
-
-                        <label htmlFor="password" className="flex text-lg text-slate-700 font-bold mb-2">
-                            Password:
-                            <FontAwesomeIcon icon={faCheck} className={validPwd ? "block text-green-300 mt-1 mx-2" : "hidden"} />
-                            <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hidden" : "block text-red-500 mt-1 mx-2"} />
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdnote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-							className="block p-3 text-lg bg-slate-200 rounded w-full mb-3"
-                        />
-                        <p id="pwdnote" className={pwdFocus && !validPwd ? "block p-2 bg-slate-200" : "hidden"}>
-                            <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
-                            8 to 24 characters.<br />
-                            Must include uppercase and lowercase letters, a number and a special character.<br />
-                            Allowed special characters:
-							<span aria-label="exclamation mark">!</span>
-							<span aria-label="at symbol">@</span>
-							<span aria-label="hashtag">#</span>
-							<span aria-label="dollar sign">$</span>
-							<span aria-label="percent">%</span>
-                        </p>
-
+						<div className="mb-4">
+							<label htmlFor="password" className="flex text-lg text-slate-700 font-bold mb-2">
+								Password:
+								<FontAwesomeIcon icon={faCheck} className={validPwd ? "block text-green-300 mt-1 mx-2" : "hidden"} />
+								<FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hidden" : "block text-red-500 mt-1 mx-2"} />
+							</label>
+							<input
+								type="password"
+								id="password"
+								onChange={(e) => setPwd(e.target.value)}
+								value={pwd}
+								required
+								aria-invalid={validPwd ? "false" : "true"}
+								aria-describedby="pwdnote"
+								onFocus={() => setPwdFocus(true)}
+								onBlur={() => setPwdFocus(false)}
+								placeholder="Password"
+								className="block p-3 text-lg bg-slate-200 rounded w-full mb-3"
+							/>
+							<p id="pwdnote" className={pwdFocus && !validPwd ? "block p-2 bg-slate-200" : "hidden"}>
+								<FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
+								8 to 24 characters.<br />
+								Must include uppercase and lowercase letters, a number and a special character.<br />
+								Allowed special characters:
+								<span aria-label="exclamation mark">!</span>
+								<span aria-label="at symbol">@</span>
+								<span aria-label="hashtag">#</span>
+								<span aria-label="dollar sign">$</span>
+								<span aria-label="percent">%</span>
+							</p>
+						</div>
 
                         <label htmlFor="confirm_pwd"  className="flex text-lg text-slate-700 font-bold mb-2">
                             Confirm Password:
@@ -166,13 +167,14 @@ const Register = () => {
                             aria-describedby="confirmnote"
                             onFocus={() => setMatchFocus(true)}
                             onBlur={() => setMatchFocus(false)}
+							placeholder="Password repeat"
 							className="block p-3 text-lg bg-slate-200 rounded w-full mb-3"
                         />
                         <p id="confirmnote" className={matchFocus && !validMatch ? "block" : "hidden"}>
                             <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
                             Must match the first password input field.
                         </p>
-                        <button disabled={!validName || !validPwd || !validMatch ? true : false} className="bg-blue-600 p-3 rounded mt-4 w-full text-white">Register</button>
+                        <button disabled={!validName || !validPwd || !validMatch ? true : false} className="rounded bg-sky-600 hover:bg-sky-700 p-2 text-lg w-full text-white">Register</button>
                     </form>
                 </section>
             )}
