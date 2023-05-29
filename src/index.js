deec8d10e71cf7bb7ce325d092b4f04b4dc17f96
Clickadelic <toby.hopp@gmail.com>
2023-05-30 01:38:32 +0200

    auskommentiert

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import Register from './components/Formus/Register'
-import Login from './components/Formus/Login'
-import ForgotPassword from './components/Formus/ForgotPassword'
+
 import Auth from './auth/Auth'
 import Error404 from './routes/Error404'
 import ProtectedRoute from './utilities/ProtectedRoute'
@@ -20,9 +18,9 @@ root.render(
 			<Routes>
 				{/* <Route path='/' element={<Home />} /> */}
 				<Route path='/auth' element={<Auth />}>
-					<Route path='login' element={<Login />} />
+					{/* <Route path='login' element={<Login />} />
 					<Route path='register' element={<Register />} />
-					<Route path='forgot-password' element={<ForgotPassword />} />
+					<Route path='forgot-password' element={<ForgotPassword />} /> */}
 				</Route>
 				<Route path="/" element={<App />}>
 					<Route path='/dashboard' element={
