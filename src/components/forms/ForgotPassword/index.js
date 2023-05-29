5441979d41132992c0af211e0a1d51ffdb7233b8
clickadelic <toby.hopp@gmail.com>
2023-05-29 02:21:17 +0200

    Forgot password

--- src/components/forms/ForgotPassword/index.js
+++ src/components/forms/ForgotPassword/index.js
@@ -1,36 +1,21 @@
 import React from 'react'
-import { useRef, useState, useEffect, } from 'react'
+import {  useState } from 'react'
 
 function ForgotPassword(){
 
-	const userRef = useRef('')
-	const errRef = useRef('')
-
-	const [user, setUser] = useState('')
-	const [pwd, setPwd] = useState('')
-	const [errMsg, setErrMsg] = useState('')
-
-	const [success, setSuccess] = useState('')
+	const [email, setEmail] = useState('')
 	
-	useEffect(() => {
-		// userRef.current.focus()
-	}, [])
-
-	useEffect(() => {
-		setErrMsg('')
-	}, [user, pwd])
-
 	const handleSubmit = async (e) => {
 		e.preventDefault()
+		console.log(email)
 	}
 	return(
 		<section className="bg-white p-4 rounded">
 			<h1 className="text-2xl mb-4">Forgot Password</h1>
-			<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
 			<form className="w-full" onSubmit={handleSubmit}>
 				<div className="mb-4">
 					<label htmlFor="username" className="flex text-medium mb-2 font-bold">E-Mail:</label>
-					<input type="text" id="username" className="block p-3 bg-slate-200 text-lg rounded w-full mb-3" autoComplete="off" placeholder="E-Mail Adresse" onChange={(e)=> setUser(e.target.value)} value={user} required />
+					<input type="text" id="username" className="block p-3 bg-slate-200 text-lg rounded w-full mb-3" autoComplete="off" placeholder="E-Mail Adresse" onChange={(e)=> setEmail(e.target.value)} required />
 				</div>
 				<button className="rounded bg-sky-600 hover:bg-sky-700 p-2 text-lg w-full text-white">Passwort wiederherstellen</button>
 			</form>
