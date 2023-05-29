f7955d0f10aaca57119b480b952b7d60cd5693f0
Clickadelic <toby.hopp@gmail.com>
2023-05-30 01:41:28 +0200

    fix try

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-
+import Register from './components/Forms/Register'
+import Login from './components/Forms/Login'
+import ForgotPassword from './components/Forms/ForgotPassword'
 import Auth from './auth/Auth'
 import Error404 from './routes/Error404'
 import ProtectedRoute from './utilities/ProtectedRoute'
@@ -18,9 +20,9 @@ root.render(
 			<Routes>
 				{/* <Route path='/' element={<Home />} /> */}
 				<Route path='/auth' element={<Auth />}>
-					{/* <Route path='login' element={<Login />} />
+					<Route path='login' element={<Login />} />
 					<Route path='register' element={<Register />} />
-					<Route path='forgot-password' element={<ForgotPassword />} /> */}
+					<Route path='forgot-password' element={<ForgotPassword />} />
 				</Route>
 				<Route path="/" element={<App />}>
 					<Route path='/dashboard' element={
