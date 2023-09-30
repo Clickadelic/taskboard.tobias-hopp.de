e18ed5645a183a1bf1d0ce1fb8b12c78dcfb9c1f
clickadelic <toby.hopp@gmail.com>
2023-10-01 00:47:18 +0200

    cleanUp Trash

--- src/components/Logo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Logo({classes, name}) {
-  return (
-	<h1 className={classes}>{name}</h1>
-  )
-}
-
-export default Logo
--- src/routes/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Dashboard() {
-	return (
-		<div>Dashboard Content</div>
-	)
-}
-
-export default Dashboard
