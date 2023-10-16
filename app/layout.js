f9c2bf42571d63a52e16b83db1478c05dfb968e8
Clickadelic <toby.hopp@gmail.com>
2023-10-16 22:07:10 +0200

    no apos

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,14 @@
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-	title: 'Toby\'s Dashboard',
-	description: 'Analytics, Uptimes, Todos',
+	title: "Toby&apos;s Dashboard",
+	description: "Analytics, Uptimes, Todos"
 };
 
 export default function RootLayout({ children }) {
-	
 	return (
 		<html lang="de">
-			<body>
-				{children}
-			</body>
+			<body>{children}</body>
 		</html>
-	)
+	);
 }
