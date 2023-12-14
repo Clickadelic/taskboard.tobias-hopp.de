0b40ffe620eef53f20bc237a0408301299098be9
clickadelic <toby.hopp@gmail.com>
2023-12-15 00:20:58 +0100

    board title field error

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,16 +1,27 @@
-"use client";
+"use client"
 
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { useFormState } from "react-dom";
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
+import { useFormState } from "react-dom"
 
 export const Form = () => {
-	const initialState = { message: null, errors: {} };
-	const [state, dispatch] = useFormState(create, initialState);
+	const initialState = { message: null, errors: {} }
+	const [state, dispatch] = useFormState(create, initialState)
 	return (
 		<form action={dispatch}>
-			<input placeholder="Enter a board title" id="title" name="title" required />
+			<div className="flex flex-col space-y-2">
+				<input placeholder="Enter a board title" id="title" name="title" required />
+				{state?.errors?.title ? (
+					<div>
+						{state?.errors.title.map((error: string) => (
+							<p key={error} className="text-rose-500">
+								{error}
+							</p>
+						))}
+					</div>
+				) : null}
+			</div>
 			<Button type="submit">Submit</Button>
 		</form>
-	);
-};
+	)
+}
