fc7cb4200a83d80ca37a6e22081ae13202d34ca2
clickadelic <toby.hopp@gmail.com>
2023-12-22 22:26:17 +0100

    input ref

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,36 +1,36 @@
-"use client";
+"use client"
 
-import { FormInput } from "@/components/form/form-input";
-import { Button } from "@/components/ui/button";
-import { Board } from "@prisma/client";
-import { useRef, ElementRef, useState } from "react";
+import { FormInput } from "@/components/form/form-input"
+import { Button } from "@/components/ui/button"
+import { Board } from "@prisma/client"
+import { useRef, ElementRef, useState } from "react"
 
 interface BoardTitleFormProps {
-	data: Board;
+	data: Board
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
-	const formRef = useRef<ElementRef<"form">>(null);
-	const inputRef = useRef<ElementRef<"input">>(null);
-	const [isEditing, setIsEditing] = useState(false);
+	const formRef = useRef<ElementRef<"form">>(null)
+	const inputRef = useRef<ElementRef<"input">>(null)
+	const [isEditing, setIsEditing] = useState(false)
 
 	const enableEditing = () => {
 		// TODO: Focus Inputs
-		setIsEditing(true);
+		setIsEditing(true)
 		setTimeout(() => {
-			inputRef.current?.focus();
-			inputRef.current?.select();
-		});
-	};
+			inputRef.current?.focus()
+			inputRef.current?.select()
+		})
+	}
 
 	const disableEditing = () => {
-		setIsEditing(false);
-	};
+		setIsEditing(false)
+	}
 
 	const onSubmit = (formData: FormData) => {
-		const title = formData.get("title") as string;
-		console.log("I am submitted!", title);
-	};
+		const title = formData.get("title") as string
+		console.log("I am submitted!", title)
+	}
 
 	if (isEditing) {
 		return (
@@ -38,15 +38,16 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 				<FormInput
 					id="title"
 					onBlur={() => {}}
+					ref={inputRef}
 					className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent"
 					defaultValue={data.title}
 				/>
 			</form>
-		);
+		)
 	}
 	return (
 		<Button onClick={enableEditing} variant="transparent" className="font-bold text-lg h-auto w-auto p-1 px-2">
 			{data.title}
 		</Button>
-	);
-};
+	)
+}
