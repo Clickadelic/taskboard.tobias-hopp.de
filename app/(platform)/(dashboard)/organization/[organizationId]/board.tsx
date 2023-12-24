dd52b2aebb0be4e3bf2a7a6e35dbc5043de0112a
Clickadelic <toby.hopp@gmail.com>
2023-12-24 01:50:34 +0100

    Add Lists

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -8,7 +8,7 @@ interface BoardProps {
 }
 
 export const Board = ({ title, id }: BoardProps) => {
-	const deleteBoardWithId = deleteBoard.bind(null, id);
+	const deleteBoardWithId = deleteBoard.bind(null, { id });
 	return (
 		<form action={deleteBoardWithId} className="flex items-center gap-x-2">
 			<p>Board Title: {title}</p>
