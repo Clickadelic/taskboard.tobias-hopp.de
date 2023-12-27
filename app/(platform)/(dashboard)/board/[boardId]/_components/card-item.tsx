0fcba6e6d324b274fb7c0e6a2e70d4c7c53bd756
Clickadelic <toby.hopp@gmail.com>
2023-12-27 01:30:52 +0100

    List order updating :D

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import { Card } from "@prisma/client";
-
+import { Draggable } from "@hello-pangea/dnd";
 interface CardItemProps {
 	data: Card;
-	index: Number;
+	index: number;
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
-	return <div className="truncate boder-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm">{data.title}</div>;
+	return (
+		<Draggable draggableId={data.id} index={index}>
+			{provided => (
+				<div
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					ref={provided.innerRef}
+					role="button"
+					className="truncate boder-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+				>
+					{data.title}
+				</div>
+			)}
+		</Draggable>
+	);
 };
