8c3f377399dd60d86b03d26f9e3ab6cb2fc79d28
Clickadelic <toby.hopp@gmail.com>
2023-12-30 01:03:51 +0100

    Strings 2

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -44,7 +44,7 @@ export const BoardList = async () => {
 				<FormPopover sideOffset={10} side="right">
 					<div role="button" className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
 						<p className="text-sm">Neues Board erstellen</p>
-						<Hint sideOffset={40} description={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade your workspace.`}>
+						<Hint sideOffset={40} description={`Klicke um ein neues Board zu erstellen.`}>
 							<HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
 						</Hint>
 					</div>
