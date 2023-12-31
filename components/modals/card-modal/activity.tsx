6a941d888c3e9ca9cf8dfb19843e5f002f112f1d
Clickadelic <toby.hopp@gmail.com>
2023-12-31 01:07:22 +0100

    Activity Item Route Fix

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -13,7 +13,7 @@ export const Activity = ({ items }: ActivityProps) => {
 		<div className="flex items-start gap-x-3 w-full">
 			<ActivityIcon className="h-5 w-5 mt-0.5 text-neutral-700" />
 			<div className="w-full">
-				<p className="font-semibold text-neutral-700 mb-2">Activity</p>
+				<p className="font-semibold text-neutral-700 mb-2">Aktivitäten</p>
 				<ol className="mt-2 space-y-4">
 					{items.map(item => (
 						<ActivityItem key={item.id} data={item} />
